38566c036a7bf874139d8650ea238ee37c6c8728
Selva Ganapathy Elangovan <153192445+Selva201998@users.noreply.github.com>
2024-03-18 21:31:38 +0100

    first commit

---
+++ src/main/java/musicianapp/musicianapp/MusicianappApplication.java
@@ -0,0 +1,13 @@
+package musicianapp.musicianapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicianappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicianappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/musicianapp/musicianapp/MusicianappApplicationTests.java
@@ -0,0 +1,13 @@
+package musicianapp.musicianapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicianappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
